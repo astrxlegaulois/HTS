7a3a8abe2a5ba49e748f706436b242214d7b2a79
astrxlegaulois <astrx@yopmail.com>
2015-11-21 14:33:58 +0100

    Premières fonctions de décryptage

--- crypt_1.py
+++ crypt_1.py
@@ -31,7 +31,7 @@ def encryptString(strString, strPassword):
     print "new intMD5Total",intMD5Total
     arrEncryptedValues=[]
     intStrlen = len(strString)
-    for i in range(0,intStrlen):
+    for i in range(intStrlen):
         print "i: ",i
         print "char to encode: ",ord(strString[i:i+1])
         print "passMd5contrib: ",int(strPasswordMD5[i%32],16)
@@ -44,4 +44,113 @@ def encryptString(strString, strPassword):
 
 # ----Fonctions de decryptage
 
+def getMd5Tables():
+    """
+    Returns a table of the 16 md5sums of the low case hexa numbers
+    """
+    ans=[]
+    for i in range(16):
+        ans.append(md5.new(str(i)).hexdigest())
+    return ans
+
+def getPotentialClearCharsAtPosition(position):
+    """
+    Returns all the possible unencrypted chars of the string at the specified position
+    """
+    ans=[]
+    if ((position % 20 == 3 or position % 20 == 7) or position % 20 == 11):
+        ans.append("-")
+    elif position%20 == 6 :
+        ans.append("O")
+    elif position%20 == 7 :
+        ans.append("E")
+    elif position%20 == 8 :
+        ans.append("M")
+    elif position%20 == 17 :
+        ans.append(".")
+    elif position%20 == 16 or position%20 == 18:
+        ans.append("1")
+    elif position%20 == 19 :
+        ans.append("\n")
+    else:
+        for i in range(0,10):
+            ans.append(str(i))
+        for i in range(ord("A"),ord("Z")+1):
+            ans.append(chr(i))
+    return ans
+
+def generateClearCharsList(length=100):
+    """
+    Returns the initial list of potential characters
+    """
+    ans=[]
+    for i in range(length):
+        ans.append(getPotentialClearCharsAtPosition(i))
+    return ans
+
+def generatePasswordMD5():
+    """
+    returns the initial list of potential hex values of the md5Password
+    """
+    ans=[]
+    for i in range(32):
+        ans.append([])
+        for j in range(16):
+            ans[-1].append(hex(j)[2])
+    return ans
+
+def refinePotentialClearChars(potentialClearChars,potPasswordMD5,potMD5Total,encryptedText,position):
+    """
+    Removes all impossible values for potentialClearChars given the potMD5Total hypothesis, the available potPasswordMD5 values and the encrypted value of the char
+    """
+    for j in [i for i in potentialClearChars[position]]:
+        found=False
+        for k in potPasswordMD5[position%32]:
+            if ord(j)==encryptedText[position]-int(k,16)+potMD5Total:
+                found=True
+                break
+        if not found:
+            potentialClearChars[position].remove(j)
+
+
+def refinePotentialPasswordMD5(potentialClearChars,potPasswordMD5,potMD5Total,encryptedText,position):
+    """
+    Removes all impossible values for potPasswordMD5 given the potentialClearChars, the potMD5Total hypothesis, and the encrypted value of the char
+    """
+    for j in [i for i in potPasswordMD5[position%32]]:
+        found=False
+        for k in potentialClearChars[position]:
+            if int(j,16)==encryptedText[position]-ord(k)+potMD5Total:
+                found=True
+                break
+        if not found:
+            potPasswordMD5[position%32].remove(j)
+    
+
+def decrypt(encryptedString):
+    """
+    Bruteforce decryption of the String
+    The md5 of the password will be recovered in the process
+    """
+    #initialisation of the tables
+
+
+    #try most probable values of potMD5Total first
+    for j in [240+int((i+1)/2)*(-1)**i for i in range(31)] + list(reversed(range(225))): #[240+int((i+1)/2)*(-1)**i for i in range(480) if 0<=240+int((i+1)/2)*(-1)**i<=255]
+        potentialClearChars=generateClearCharsList(len(encryptedString))
+        potPasswordMD5=generatePasswordMD5()
+        print "potMD5Total:",potMD5Total
+        for position in range(len(encryptedString)):
+            print "position:", position
+            refinePotentialClearChars(potentialClearChars,potPasswordMD5,potMD5Total,encryptedString,position)
+            if not potentialClearChars[position]:
+                break
+            refinePotentialPasswordMD5(potentialClearChars,potPasswordMD5,potMD5Total,encryptedString,position)
+            if not potPasswordMD5[position%32]:
+                break
+            potMD5Total=iterate #!!!!!!!!!!!!!!!!
+
+    
+    
+    return strString
 # ----Main
